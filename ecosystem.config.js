8c37a3b3eb54ad59beda6dbbecdee5263c9f812b
foxsis <va.four2016@gmail.com>
2022-12-22 21:07:48 +0300

    fix conf pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,11 @@ module.exports = {
     apps: [
       {
         name: 'CRM',
+        port: 3333,
         exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
+        instances: 'max',
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start'
-      }
+      },
     ]
   }
